410c764b5325820c713b48f064e8f401819e2cf4
Ojani <ojanifigueroa.of@gmail.com>
2026-04-19 04:11:46 -0400

    Put sports info dummy data into its own file

---
+++ tu-deporte-aqui/app/teams/teamInfo.ts
@@ -0,0 +1,197 @@
+export interface TeamInfo {
+  name: string,
+  image: string,
+  wins: number,
+  losses: number
+}
+
+export type Sport = "Basketball" | "Baseball" | "Volleyball";
+
+export const teamInfosBB: TeamInfo[] = [
+  {
+    "name": "Metro Stars",
+    "image": "/team-images/basketball/t1.jpeg",
+    "wins": 3,
+    "losses": 0
+  },
+  {
+    "name": "Coastline Breakers",
+    "image": "/team-images/basketball/t2.jpeg",
+    "wins": 3,
+    "losses": 2
+  },
+  {
+    "name": "Summit Peaks",
+    "image": "/team-images/basketball/t3.jpeg",
+    "wins": 0,
+    "losses": 2
+  },
+  {
+    "name": "Harbor Giants",
+    "image": "/team-images/basketball/t4.jpeg",
+    "wins": 2,
+    "losses": 2
+  },
+  {
+    "name": "Empire Archers",
+    "image": "/team-images/basketball/t5.jpeg",
+    "wins": 1,
+    "losses": 3
+  },
+  {
+    "name": "Canyon Coyotes",
+    "image": "/team-images/basketball/t6.jpeg",
+    "wins": 2,
+    "losses": 3
+  },
+  {
+    "name": "Iron City Forge",
+    "image": "/team-images/basketball/t7.jpeg",
+    "wins": 4,
+    "losses": 1
+  },
+  {
+    "name": "Bay Area Pilots",
+    "image": "/team-images/basketball/t8.jpeg",
+    "wins": 1,
+    "losses": 1
+  },
+  {
+    "name": "Ridge Rebels",
+    "image": "/team-images/basketball/t9.jpeg",
+    "wins": 0,
+    "losses": 2
+  },
+  {
+    "name": "Skyline Comets",
+    "image": "/team-images/basketball/t1.jpeg",
+    "wins": 3,
+    "losses": 3
+  }
+]
+
+export const teamInfosBaseB: TeamInfo[] = [
+  {
+    "name": "Midtown Blue Sox",
+    "image": "/team-images/baseball/t1.jpeg",
+    "wins": 12,
+    "losses": 4
+  },
+  {
+    "name": "Coastal Otters",
+    "image": "/team-images/baseball/t2.jpeg",
+    "wins": 9,
+    "losses": 7
+  },
+  {
+    "name": "Mountain Cats",
+    "image": "/team-images/baseball/t3.jpeg",
+    "wins": 5,
+    "losses": 11
+  },
+  {
+    "name": "Bayside Mariners",
+    "image": "/team-images/baseball/t4.jpeg",
+    "wins": 10,
+    "losses": 6
+  },
+  {
+    "name": "Capital Senators",
+    "image": "/team-images/baseball/t5.jpeg",
+    "wins": 8,
+    "losses": 8
+  },
+  {
+    "name": "Desert Rattlers",
+    "image": "/team-images/baseball/t6.jpeg",
+    "wins": 7,
+    "losses": 9
+  },
+  {
+    "name": "Steel Mill Grays",
+    "image": "/team-images/baseball/t7.jpeg",
+    "wins": 14,
+    "losses": 2
+  },
+  {
+    "name": "Pacific Aviators",
+    "image": "/team-images/baseball/t8.jpeg",
+    "wins": 11,
+    "losses": 5
+  },
+  {
+    "name": "Valley Vipers",
+    "image": "/team-images/baseball/t9.jpeg",
+    "wins": 3,
+    "losses": 13
+  },
+  {
+    "name": "Skyline Redwings",
+    "image": "/team-images/baseball/t1.jpeg",
+    "wins": 6,
+    "losses": 10
+  }
+]
+
+export const teamInfosVB: TeamInfo[] = [
+  {
+    "name": "Metro High-Risers",
+    "image": "/team-images/volleyball/t1.jpeg",
+    "wins": 15,
+    "losses": 3
+  },
+  {
+    "name": "Coastal Wave",
+    "image": "/team-images/volleyball/t2.jpeg",
+    "wins": 12,
+    "losses": 6
+  },
+  {
+    "name": "Summit Peaks",
+    "image": "/team-images/volleyball/t3.jpeg",
+    "wins": 4,
+    "losses": 14
+  },
+  {
+    "name": "Bayside Breakers",
+    "image": "/team-images/volleyball/t4.jpeg",
+    "wins": 10,
+    "losses": 8
+  },
+  {
+    "name": "Capital Impact",
+    "image": "/team-images/volleyball/t5.jpeg",
+    "wins": 9,
+    "losses": 9
+  },
+  {
+    "name": "Desert Heat",
+    "image": "/team-images/volleyball/t6.jpeg",
+    "wins": 7,
+    "losses": 11
+  },
+  {
+    "name": "Iron City Spikes",
+    "image": "/team-images/volleyball/t7.jpeg",
+    "wins": 17,
+    "losses": 1
+  },
+  {
+    "name": "Pacific Flight",
+    "image": "/team-images/volleyball/t8.jpeg",
+    "wins": 11,
+    "losses": 7
+  },
+  {
+    "name": "Valley Volleys",
+    "image": "/team-images/volleyball/t9.jpeg",
+    "wins": 2,
+    "losses": 16
+  },
+  {
+    "name": "Skyline Storm",
+    "image": "/team-images/volleyball/t1.jpeg",
+    "wins": 8,
+    "losses": 10
+  }
+]
